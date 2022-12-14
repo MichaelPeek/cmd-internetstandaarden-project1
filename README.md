2fabcfbc5ddc1880b134eb2c2daa3d9709a7eb07
Michael Peek <michaelpeek132@gmail.com>
2022-12-14 17:44:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Website michael
+Website url: https://
+
+## Bronnen:
+1. Slideshow: https://www.w3schools.com/howto/howto_js_slideshow.asp
+1. Contact icons: https://fontawesome.com/icons
+1. Footer aan onderkant scherm: https://www.freecodecamp.org/news/how-to-keep-your-footer-where-it-belongs-59c6aa05c59c/
---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+// Start slide show
+// Source: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slides-1");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // 3 seconds
+}
+
+// End slide show
